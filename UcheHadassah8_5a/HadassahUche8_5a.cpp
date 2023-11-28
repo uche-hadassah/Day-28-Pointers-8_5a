8d1ce274ba9b5603dc96153775f1460845fb50ce
Hadassah Uche <uhadassah1@gmail.com>
2023-11-28 14:35:09 +0200

    Ive created ny rough draft. There are tons of errors so time to start correcting

--- UcheHadassah8_5a/HadassahUche8_5a.cpp
+++ UcheHadassah8_5a/HadassahUche8_5a.cpp
@@ -1,5 +1,28 @@
 /*Repeat exercise 3a  from exercise sheet 6, using pointers.
- 3a)) Write a program which reads a word and checks if it is a palindrome 
+3a)) Write a program which reads a word and checks if it is a palindrome 
 by outputting the appropriate message: This is a palindrome accordingly. A 
 word is a palindrome if the letters spell the same message when written both forward 
 and backward. For example: madam, anna, bob, noon are palindromes.*/
+#include<iostream>
+#include<cstring>
+using namespace std;
+const int MAX = 100;
+int main()
+{
+	char word[MAX];
+	char* p;
+	int i;
+	cout << "Enter a word:";
+	cin >> word;
+	p = word;
+	for (i = 0; word[i] != '\0'; i++);
+	while (*p <= i)
+	{
+		if (*p != word[i])
+			cout << "This is not a palindrome!";
+		if (*p == word[i])
+			p++; i--;
+		if (*p >= i)
+			cout << "It is a palindrome.";
+	}
+}
