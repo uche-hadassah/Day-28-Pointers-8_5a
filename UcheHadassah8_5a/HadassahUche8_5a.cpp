4ab0132038443e180dfe2dde922992799de9ee2b
Hadassah Uche <uhadassah1@gmail.com>
2023-11-28 14:12:34 +0200

    Add project files.

---
+++ UcheHadassah8_5a/HadassahUche8_5a.cpp
@@ -0,0 +1,5 @@
+/*Repeat exercise 3a  from exercise sheet 6, using pointers.
+ 3a)) Write a program which reads a word and checks if it is a palindrome 
+by outputting the appropriate message: This is a palindrome accordingly. A 
+word is a palindrome if the letters spell the same message when written both forward 
+and backward. For example: madam, anna, bob, noon are palindromes.*/
